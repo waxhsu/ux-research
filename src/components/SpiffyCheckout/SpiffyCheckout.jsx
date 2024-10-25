c6fa4b7088d4d18bab80d7cf2a35767b30a4304b
Anton <shelkovnikov9108@gmail.com>
2024-10-25 10:59:59 -0400

    removed the first test iframe

--- src/components/SpiffyCheckout/SpiffyCheckout.jsx
+++ src/components/SpiffyCheckout/SpiffyCheckout.jsx
@@ -8,25 +8,9 @@ export default function SpiffyCheckout() {
     return (
         <div className="spiffyCheckout-box">
             <h2>Join the October UX Metrics Cohort</h2>
-            <ReactIframe
-                url="https://centercentre.spiffy.co/checkout/ux-metrics-oct"
-                width="100%"
-                height="800px"
-            />
+
             <spiffy-checkout url="https://centercentre.spiffy.co/checkout/ux-metrics-oct" ></spiffy-checkout>
-            {/* <iframe
-                src="https://centercentre.spiffy.co/checkout/ux-metrics-oct"
-                width="100%"
-                height="800px"
-                frameBorder="0"
-                allow="payment"
-                title="Spiffy Checkout"
-            /> */}
-            {/* <ReactIframe
-                url="https://blog.openreplay.com/"
-                width="500px"
-                height="300px"
-            /> */}
+
 
         </div>
 
