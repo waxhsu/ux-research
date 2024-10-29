81c5841629170e4d9c556dbfc296effada9c4e1e
waxhsu <98721786+waxhsu@users.noreply.github.com>
2024-10-29 12:59:27 -0400

    Max test1

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
         <Route path="/teams" element={<Teams />} />
         <Route path="/topics" element={<Topics />} />
 
+
         <Route path="/octoberschedule" element={<ScheduleOctober />} />
         <Route path="/novemberschedule" element={<ScheduleNovember />} />
         <Route path="/decemberschedule" element={<ScheduleDecember />} />
